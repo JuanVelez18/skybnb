e921efa3f0bd2d0bc09f639c097c5db3024c5017
Juan Esteban Velez <juanvelez1113670@correo.itm.edu.co>
2025-03-03 20:26:31 -0500

    Agregar clase Personas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+public class Personas
+{
+    public int Id { get; set; }
+    public string? Cedula { get; set; }
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public DateOnly FechaNacimiento { get; set; }
+    public string? Email { get; set; }
+    public string? Telefono { get; set; }
+}
