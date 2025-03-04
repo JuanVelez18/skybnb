69228c2682e4603c4109143ba6d6fe81a0592b60
Juan Esteban Velez <juanvelez1113670@correo.itm.edu.co>
2025-03-03 20:38:39 -0500

    Agregar clases Alojamientos y Tipos_Alejamientos

--- Program.cs
+++ Program.cs
@@ -26,3 +26,28 @@ public class Ciudades
     public int Id { get; set; }
     public string? Nombre { get; set; }
 }
+
+public class Tipos_Alejamientos
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+}
+
+public class Alojamientos
+{
+    public int Id { get; set; }
+    public int Anfitrion { get; set; }
+    public string? Descripcion { get; set; }
+    public int Direccion { get; set; }
+    public int Ciudad { get; set; }
+    public int Tipo { get; set; }
+    public double CostoNoche { get; set; }
+    public int CantidadHabitaciones { get; set; }
+    public int CantidadBaños { get; set; }
+
+    public Personas? _Anfitrion { get; set; }
+    public Direcciones? _Direccion { get; set; }
+    public Ciudades? _Ciudad { get; set; }
+    public Tipos_Alejamientos? _Tipo { get; set; }
+
+}
